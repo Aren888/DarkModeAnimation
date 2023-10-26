51b31b0e15c62799a2ef3fd54749aa123afecad2
Aren888 <aren.musayelyan555@gmail.com>
2023-10-26 11:41:07 +0400

    Added Xcode Project

---
+++ DarkModeAnimation/DarkModeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkModeAnimationApp.swift
+//  DarkModeAnimation
+//
+//  Created by Solicy Ios on 26.10.23.
+//
+
+import SwiftUI
+
+@main
+struct DarkModeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
